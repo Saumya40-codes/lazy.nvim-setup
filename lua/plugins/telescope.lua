cb105934544506faef2d33427e3fb0c6076051be
Saumya Shah <saumyabshah90@gmail.com>
2025-05-19 10:16:36 +0530

    add file ignore patterns
    Signed-off-by: Saumya Shah <saumyabshah90@gmail.com>

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,17 @@ return {
   tag = "0.1.8",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    require("telescope").setup({})
+    require("telescope").setup({
+      defaults = {
+        file_ignore_patterns = {
+          "node_modules",
+          "go.sum",
+          "%.lock", -- ignores yarn.lock, package-lock.json etc.
+          "%.min%.js", -- ignores minified JS files
+        },
+      },
+    })
+
     vim.keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "Live Grep" })
   end,
 }
